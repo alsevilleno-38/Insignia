49ff08dedf57f7f9c4fd2b2f2236208e5e812f50
aliengamer38 <aliengamer38@gmail.com>
2020-09-05 22:18:33 +0800

    updated

--- App/core/main.cpp
+++ App/core/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+namespace sign
+{
+    _declspec(dllexport) void transact();
+    _declspec(dllexport) void display();
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
+    sign::transact();
+    sign::display();
 }
